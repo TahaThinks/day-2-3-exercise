12a7f320292d3cf7836939aa42dd0e933ea82ebc
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-10-30 20:21:12 +0000

    Created an Application that gives you the Life remaining for you to complete 90 years

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ months_left = years_left*12
 weeks_left = months_left*52
 days_left = weeks_left*365
 
-print(f"You have {days_left}")
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left")
 
 
 
